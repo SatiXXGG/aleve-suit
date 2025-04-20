d3157ce6beddd539e00c9707040c466082355f24
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2025-04-19 23:18:57 -0600

    fix: queue export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Character } from "./Character";
 import { Path } from "./Find";
+import { Queue } from "./Queue";
 import { Signal } from "./Signal";
 import { State } from "./State";
 import { Thread } from "./Thread";
 import { Time } from "./Time";
 import { Value } from "./Value";
 
-export { Path as Find, Signal, Thread, Time, Value, Character, State };
+export { Path as Find, Signal, Thread, Time, Value, Character, State, Queue };
